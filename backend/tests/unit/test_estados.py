eeb93bcf723945380341c05836df8cc32e50d510
jhoan2000 <jhoanpa57@gmail.com>
2026-06-30 00:12:30 -0500

    Test unitarios v3

--- backend/tests/unit/test_estados.py
+++ backend/tests/unit/test_estados.py
@@ -1,23 +1,33 @@
 from utils.estados import *
 
 # Solicitud
-def test_estado_solicitud():
+def test_estado_solicitud_abierta():
     assert EstadoSolicitud.ABIERTA == "ABIERTA"
+def test_estado_solicitud_aceptada():
     assert EstadoSolicitud.ACEPTADA == "ACEPTADA"
+def test_estado_solicitud_en_curso():
     assert EstadoSolicitud.EN_CURSO == "EN_CURSO"
+def test_estado_solicitud_finalizada():
     assert EstadoSolicitud.FINALIZADA == "FINALIZADA"
+def test_estado_solicitud_cancelada():
     assert EstadoSolicitud.CANCELADA == "CANCELADA"
 
 # Oferta
-def test_estado_oferta():
+def test_estado_oferta_pendiente():
     assert EstadoOferta.PENDIENTE == "PENDIENTE"
+def test_estado_oferta_aceptada():
     assert EstadoOferta.ACEPTADA == "ACEPTADA"
+def test_estado_oferta_rechazada():
     assert EstadoOferta.RECHAZADA == "RECHAZADA"
+def test_estado_oferta_cancelada():
     assert EstadoOferta.CANCELADA == "CANCELADA"
 
 # Domiciliario
-def test_estado_domiciliario():
+def test_estado_domiciliario_disponible():
     assert EstadoDomiciliario.DISPONIBLE == "DISPONIBLE"
+def test_estado_domiciliario_en_servicio():
     assert EstadoDomiciliario.EN_SERVICIO == "EN_SERVICIO"
+def test_estado_domiciliario_desconectado():
     assert EstadoDomiciliario.DESCONECTADO == "DESCONECTADO"
+def test_estado_domiciliario_suspendido():
     assert EstadoDomiciliario.SUSPENDIDO == "SUSPENDIDO"
